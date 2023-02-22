57488e19e3a4f0790e72cdf480ffc2bd2506d49e
lzb <1325764805@qq.com>
2023-02-22 14:57:21 +0800

    first commit

---
+++ src/main/java/com/xuexi/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xuexi.git;
+
+/**
+ * @Date: 2023/2/22
+ * @Author: 13257
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
